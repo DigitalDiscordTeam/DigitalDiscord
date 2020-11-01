f7d29f30402a27d6e34c463b7a54487d310cddb3
SirWolf <69642876+SirWolfi@users.noreply.github.com>
2020-11-01 21:53:01 +0100

    Real-delete
    there were some bugs in the last delete. Now here is the real

--- Events.cpp
+++
@@ -1,5 +0,0 @@
-#include "InternalEvents.h"
-
-bool Event::Spamc::toggleb = false;
-bool Event::Testc::toggleb = false;
-bool Event::CheckCorruptionc::toggleb = false;
--- setup.h
+++
@@ -1,8 +0,0 @@
-#ifndef SETUP_H
-#define SETUP_H
-
-#ifndef _SURE_READ_
-#define _SURE_READ_
-#endif
-
-#endif
